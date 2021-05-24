fb79bc49ed41fb2053cc2f75dd12e4594bcce88a
axepeartree <joaoeduardo.smp@gmail.com>
2021-05-24 01:07:49 -0300

    save matched word to variable

--- src/lz77_decomp.vhdl
+++ src/lz77_decomp.vhdl
@@ -18,34 +18,41 @@ end entity lz77_decomp;
 architecture behaviour of lz77_decomp is
     -- array of 8191 bytes
     type buf_mem_type is array (2**13 - 1 downto 0) of std_logic_vector(7 downto 0);
-    type carry_bytes_type is array (6 downto 0) of std_logic_vector(7 downto 0);
+    type match_word_type is array(6 downto 0) of std_logic_vector(7 downto 0);
 
     signal s_buffer:            buf_mem_type;
-    signal s_next_buffer_head:  std_logic_vector(12 downto 0) := "1111111111111"; -- next head of the circular buffer
-    signal s_curr_buffer_head:  std_logic_vector(12 downto 0) := "1111111111111"; -- current head of the circular buffer
-    signal s_match_position:    integer := 0; -- only for debugging reasons
-    signal s_carry_byte_count:  unsigned (3 downto 0) := b"0000";
+    signal s_next_buffer_head:  std_logic_vector(12 downto 0) := "0000000000000"; -- next head of the circular buffer
+    signal s_curr_buffer_head:  std_logic_vector(12 downto 0) := "0000000000000"; -- current head of the circular buffer, useful for debugging
     signal s_write:             std_logic := '0';
 begin
     process(clock) is
-        variable v_curr_buffer_head:    std_logic_vector(12 downto 0) := "1111111111111";
+        variable v_curr_buffer_head:    std_logic_vector(12 downto 0) := "0000000000000";
         variable v_match_position:      integer := 0;
-        variable v_carry_byte_count:    unsigned (3 downto 0) := b"0000";
+        variable v_match_word:          match_word_type;
         variable v_write:               std_logic := '0';
     begin
         if rising_edge(clock) and enable = '1' then
-            v_carry_byte_count := s_carry_byte_count + unsigned(length);
             v_curr_buffer_head := s_next_buffer_head;
-            v_match_position := to_integer(unsigned(v_curr_buffer_head)) - to_integer(unsigned(offset));
 
-            if v_carry_byte_count >= 8 then
-                v_carry_byte_count := v_carry_byte_count - 8;
-                v_write := '1';
+            -- set match word
+            v_match_position := to_integer(unsigned(v_curr_buffer_head)) - to_integer(unsigned(offset));
+            if v_match_position < 0 then
+                v_match_position := 8192 + v_match_position;
             else
-                v_write := '0';
+                v_match_position := v_match_position;
             end if;
 
-            -- get buffer's next position
+            -- copy from buffer to match word
+            for i in 0 to 6 loop
+                if i + v_match_position > 8191 and i <= unsigned(length) then
+                    v_match_word(i) := s_buffer(v_match_position + i - 8192);
+                elsif i <= unsigned(length) then
+                    v_match_word(i) := s_buffer(v_match_position + i);
+                else
+                end if;
+            end loop;
+
+            -- set buffer's current and next position using circular buffer logic
             if unsigned(v_curr_buffer_head) + unsigned(length) + 1 > 8191 then
                 s_next_buffer_head
                     <= std_logic_vector((unsigned(v_curr_buffer_head) + unsigned(length)) - 8191);
@@ -54,14 +61,7 @@ begin
                     <= std_logic_vector((unsigned(v_curr_buffer_head) + unsigned(length)) + 1);
             end if;
 
-            if v_match_position < 0 then
-                s_match_position <= 8192 + v_match_position;
-            else
-                s_match_position <= v_match_position;
-            end if;
-
             s_curr_buffer_head <= v_curr_buffer_head;
-            s_carry_byte_count <= v_carry_byte_count;
             s_write <= v_write;
         end if;
         end process;
