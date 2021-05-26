8a1a53875d86575d16975a295131890d32eb0f6f
axepeartree <joaoeduardo.smp@gmail.com>
2021-05-25 22:02:49 -0300

    Fixed a bug with the build script

--- build.sh
+++ build.sh
@@ -6,10 +6,7 @@ echo "Building decoder"
 ghdl -a src/lz77_decoder.vhdl &&
 ghdl -e lz77_decoder &&
 
-echo "Building tb"
-
-ghdl -a test/lz77_decoder_tb.vhdl &&
-ghdl -e lz77_decoder_tb &&
+echo "Building testbench"
 
 ghdl -a test/testbench.vhdl &&
 ghdl -e testbench
